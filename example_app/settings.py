8b46cda2bf5083b8e3cf6cb3c53a1b257b83e5ec
vedantnarayan <vedant.006@gmail.com>
2017-08-28 10:09:38 +0530

    settings.py changes

--- example_app/settings.py
+++ example_app/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'fsch+6!=q+@ol&%0x!nwdl@48^ixbd4clx5f1i!5n^66y+pmn*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quiet-anchorage-41783.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','quiet-anchorage-41783.herokuapp.com']
 
 
 # Application definition
